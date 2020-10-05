d486cfd9a476c22ee6c1702a646d6c59a88062e0
Aris <64918822+Arisamiga@users.noreply.github.com>
2020-10-05 22:22:01 +0100

    Added extra music + more shapes + loops

--- main.py
+++ main.py
@@ -1,34 +1,204 @@
-def on_mes_broadcast_general_id_remote_control_evt_pause():
-    music.stop_melody(MelodyStopOptions.ALL)
-control.on_event(EventBusSource.MES_BROADCAST_GENERAL_ID,
-    EventBusValue.MES_REMOTE_CONTROL_EVT_PAUSE,
-    on_mes_broadcast_general_id_remote_control_evt_pause)
+def Symbols():
+    basic.show_icon(IconNames.EIGTH_NOTE)
+    for index in range(4):
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            """)
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            """)
+        basic.show_leds("""
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            """)
+        basic.show_leds("""
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            """)
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            """)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            """)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            """)
+        basic.show_leds("""
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            """)
+        basic.show_leds("""
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            """)
+        basic.show_leds("""
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            """)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            """)
+        basic.show_leds("""
+            # . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            """)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # . . .
+            """)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # # .
+            """)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . #
+            """)
+        basic.show_leds("""
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . .
+            . . . . .
+            """)
+        basic.show_leds("""
+            . . . # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+        basic.show_leds("""
+            . # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+    for index2 in range(4):
+        basic.show_leds("""
+            . # # # .
+            # # # . .
+            # # . . #
+            # # # . .
+            . # # # .
+            """)
+        basic.show_leds("""
+            . # # # .
+            # # # . .
+            # # . # .
+            # # # . .
+            . # # # .
+            """)
+        basic.show_leds("""
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            """)
 
 def on_bluetooth_connected():
+    basic.clear_screen()
     basic.show_icon(IconNames.YES)
     music.play_melody("E B E F B A E C5 ", 120)
+    while True:
+        Symbols()
 bluetooth.on_bluetooth_connected(on_bluetooth_connected)
 
 def on_bluetooth_disconnected():
+    basic.clear_screen()
     basic.show_icon(IconNames.NO)
     music.stop_melody(MelodyStopOptions.ALL)
     music.play_melody("E B E F A G E C ", 120)
     bluetooth.start_button_service()
 bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
 
-def on_mes_broadcast_general_id_alert_evt_play_sound():
-    music.set_volume(127)
-control.on_event(EventBusSource.MES_BROADCAST_GENERAL_ID,
-    EventBusValue.MES_ALERT_EVT_PLAY_SOUND,
-    on_mes_broadcast_general_id_alert_evt_play_sound)
+def on_button_pressed_b():
+    basic.clear_screen()
+    basic.show_string("Hello! The Temperature is")
+    basic.show_number(input.temperature())
+    basic.show_string("C")
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
-basic.show_leds("""
-    # . # . #
-    # . # . #
-    # # # . #
-    # . # . #
-    # . # . #
-    """)
+device_name = "Aris's Speaker"
+
+basic.show_string(control.device_name())
+for index3 in range(4):
+    basic.show_leds("""
+        # . # . #
+        # . # . #
+        # # # . #
+        # . # . #
+        # . # . #
+        """)
+    basic.show_icon(IconNames.HAPPY)
+    basic.show_leds("""
+        # . # . #
+        # . # . #
+        # # # . #
+        # . # . #
+        # . # . #
+        """)
 bluetooth.start_button_service()
 
 def on_forever():
--- main.ts
+++ main.ts
@@ -160,6 +160,7 @@ function Symbols () {
     }
 }
 bluetooth.onBluetoothConnected(function () {
+    basic.clearScreen()
     basic.showIcon(IconNames.Yes)
     music.playMelody("E B E F B A E C5 ", 120)
     while (true) {
@@ -167,18 +168,36 @@ bluetooth.onBluetoothConnected(function () {
     }
 })
 bluetooth.onBluetoothDisconnected(function () {
+    basic.clearScreen()
     basic.showIcon(IconNames.No)
     music.stopMelody(MelodyStopOptions.All)
     music.playMelody("E B E F A G E C ", 120)
     bluetooth.startButtonService()
 })
-basic.showLeds(`
-    # . # . #
-    # . # . #
-    # # # . #
-    # . # . #
-    # . # . #
-    `)
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString("Hello! The Temperature is")
+    basic.showNumber(input.temperature())
+    basic.showString("C")
+})
+music.playMelody("G A F G A F B C5 ", 300)
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # # # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.showIcon(IconNames.Happy)
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # # # . #
+        # . # . #
+        # . # . #
+        `)
+}
 bluetooth.startButtonService()
 basic.forever(function () {
 	
