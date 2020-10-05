a6f33d35b4d55118766bfee4ee2abd48a59784a7
Aris <64918822+Arisamiga@users.noreply.github.com>
2020-10-05 19:10:37 +0100

    Added symbols for music

--- main.ts
+++ main.ts
@@ -1,18 +1,176 @@
-control.onEvent(EventBusSource.MES_BROADCAST_GENERAL_ID, EventBusValue.MES_REMOTE_CONTROL_EVT_PAUSE, function () {
-    music.stopMelody(MelodyStopOptions.All)
-})
+function Symbols () {
+    basic.showIcon(IconNames.EigthNote)
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # # # .
+            # # # . .
+            # # . . #
+            # # # . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # # # .
+            # # # . .
+            # # . # .
+            # # # . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    }
+}
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.Yes)
     music.playMelody("E B E F B A E C5 ", 120)
+    while (true) {
+        Symbols()
+    }
 })
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.No)
     music.stopMelody(MelodyStopOptions.All)
     music.playMelody("E B E F A G E C ", 120)
     bluetooth.startButtonService()
-})
-control.onEvent(EventBusSource.MES_BROADCAST_GENERAL_ID, EventBusValue.MES_ALERT_EVT_PLAY_SOUND, function () {
-    music.setVolume(127)
 })
 basic.showLeds(`
     # . # . #
